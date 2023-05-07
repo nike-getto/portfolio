8f0fec433d480f0af17d53149c7acd78032fe4d2
nike-getto <savastanisic7@gmail.com>
2023-05-07 17:09:46 +0200

    Lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 	return (
 		<div className='bg-[#242424] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
 			<Head>
-				<title>Sava's Portfolio</title>
+				<title>Sava Portfolio</title>
 			</Head>
 
 			<Header />
